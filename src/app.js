e34f526fbd37e9cf82b0f24f9b83ff39ccf72fec
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2023-11-20 17:18:59 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let cards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * cards.length);
+    let randomSuit = Math.floor(Math.random() * suits.length);
+
+    document.getElementById("card-top").innerHTML = suits[randomSuit];
+    document.getElementById("card-img").innerHTML = cards[randomNumber];
+    document.getElementById("card-bottom").innerHTML = suits[randomSuit];
+    document.getElementById("card-bottom").style.transform = "rotate(180deg)";
+
+    if (suits[randomSuit] === "♦" || suits[randomSuit] === "♥") {
+      document.getElementById("card-top").style.color = "#d00";
+      document.getElementById("card-bottom").style.color = "#d00";
+    } else {
+      document.getElementById("card-top").style.color = "#000000";
+      document.getElementById("card-bottom").style.color = "#000000";
+    }
+  }
+  document.getElementById("button").addEventListener("click", function() {
+    getRandomCard();
+  });
+  setInterval(getRandomCard, 3000);
 };
